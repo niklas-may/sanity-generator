c9fec2e4e9ecedbb8f14841195d75d12a55c72ee
Niklas May <post@niklas-may.de>
2023-09-06 18:04:26 +0200

    build

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,18 @@
+#! /usr/bin/env node
+
 import path from "path";
 import { program } from "commander";
 import { main } from "./main";
+import { register } from "ts-node";
+
+register({
+  compilerOptions: {
+    module: "CommonJS",
+  },
+  require: ["tsconfig-paths/register"],
+  typeCheck: false,
+  transpileOnly: true,
+});
 
 program
   .name("Sanity Generator")
