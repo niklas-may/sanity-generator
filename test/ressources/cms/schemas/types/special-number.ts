d602ce8e03b7de35a5c41a622a5605863c5a6458
Niklas May <post@niklas-may.de>
2023-09-24 11:08:05 +0200

    fix: tsconfig path alias in test cms

--- test/ressources/cms/schemas/types/special-number.ts
+++ test/ressources/cms/schemas/types/special-number.ts
@@ -1,10 +1,10 @@
-import { Resolver } from "../../../../src/types";
+import type {Resolver} from 'sanity-generator/types'
 
 export const specialNumberSchema = {
-  type: "number",
-  name: "specialNumber",
-};
+  type: 'number',
+  name: 'specialNumber',
+}
 
 export const specialNumberResolver: Resolver = (name: string) => /* groq */ `
     "${name}-special-number": ${name}
-`;
+`
